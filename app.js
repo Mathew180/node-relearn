3687ad5089fec9bb1c1769667c6c752a3478afdb
mathew180 <mathewadavuruku@gmail.com>
2024-02-11 15:35:03 +0100

    node relearn1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -20,6 +22,19 @@ app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-console.log(`Server is running......`)
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, ()=>{
+          console.log(`Server is running......`)
 })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+// app.listen(port, ()=>{
+// console.log(`Server is running......`)
+// })
